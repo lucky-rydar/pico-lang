68500ed092121dc54bd9768fffc03328fb6b4639
Mikhail Didur <mihasyaterminator@gmail.com>
2021-06-05 15:38:57 +0300

    Defined base functions for lexer Added some setting to .code dir

---
+++ src/Lexer.cpp
@@ -0,0 +1,36 @@
+#include "Lexer.h"
+
+Lexer::Lexer()
+{
+
+}
+
+Lexer::Lexer(string input)
+{
+    
+}
+
+void Lexer::setInput(string input)
+{
+
+}
+
+string Lexer::getInput()
+{
+    return "";
+}
+
+void Lexer::setDelimiter(string input)
+{
+
+}
+
+string Lexer::getDelimiter()
+{
+    return "";
+}
+
+vector<string> Lexer::parse()
+{
+    return vector<string>();
+}
---
+++ src/Lexer.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <regex>
+
+using namespace std;
+
+class Lexer
+{
+private:
+    string input;
+    string delimiter;
+    vector<string> tokens;
+
+public:
+    Lexer();
+    Lexer(string input);
+
+    void setInput(string input);
+    string getInput();
+
+    void setDelimiter(string delimiter);
+    string getDelimiter();
+
+    vector<string> parse();
+};
