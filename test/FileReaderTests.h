65e1698b808ae3ac4bbf0e895c0ed8aaa02871d6
Mykhailo Didur <mihasyaterminator@gmail.com>
2021-06-19 16:29:30 +0300

    Added tests for file reader class

---
+++ test/FileReaderTests.h
@@ -0,0 +1,35 @@
+#include "gtest/gtest.h"
+#include "FileReader.h"
+
+TEST(FileReader, readAsText)
+{
+    string filename = "hello";
+    string text = "Hello world";
+
+    ofstream f(filename);
+    f << text << endl;
+    f.close();
+
+    ASSERT_NO_THROW(FileReader::readAsText(filename));
+    
+    ASSERT_EQ(FileReader::readAsText(filename), text);
+
+    remove(filename.c_str());
+}
+
+TEST(FileReader, readAsBytes)
+{
+    string filename = "hello";
+    string text = "123 -12 44";
+    vector<int> mustBe = {123, -12, 44};
+
+    ofstream f(filename);
+    f << text << endl;
+    f.close();
+
+    ASSERT_NO_THROW(FileReader::readAsBytes(filename));
+    
+    ASSERT_EQ(FileReader::readAsBytes(filename), mustBe);
+
+    remove(filename.c_str());
+}
