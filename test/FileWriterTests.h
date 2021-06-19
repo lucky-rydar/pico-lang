578d34b1db9bf94df6915b5b7050851c6536ba4b
Mykhailo Didur <mihasyaterminator@gmail.com>
2021-06-19 17:32:04 +0300

    Fixed file writer tests to remove created file after test

--- test/FileWriterTests.h
+++ test/FileWriterTests.h
@@ -4,8 +4,10 @@
 TEST(FileWriter, checkWrite)
 {
     vector<int> bytes({1, 2, 3});
-    string filename = "a.pl";
+    string filename = "a.ple";
     FileWriter::writeBytes(filename, bytes);
     
     ASSERT_EQ(FileReader::readAsBytes(filename), bytes);
+
+    remove(filename.c_str());
 }
