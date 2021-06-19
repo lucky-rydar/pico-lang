bdd76c9a5ec72d6d8870bff50bb6013d1f66b516
Mykhailo Didur <mihasyaterminator@gmail.com>
2021-06-19 18:09:07 +0300

    Integrated console class with main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
-#include "VirtualMachine.h"
+#include "Console.h"
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-    cout << "Hello pico-lang" << endl;
+    if(argc == 1)
+    {
+        cout << "Not enough arguments" << endl;
+        return -1;
+    }
+
+    vector<string> command;
+    for(int i = 1; i < argc; i++)
+        command.push_back(string(argv[i]));
+
+    try
+    {
+        Console c;
+        c.process(command);
+    }
+    catch(runtime_error re)
+    {
+        cout << "Something is wrong: " << re.what() << endl;
+        return -1;
+    }
+
     return 0;
 }
