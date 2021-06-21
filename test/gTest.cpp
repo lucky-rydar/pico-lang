efa91b79c70fa31f5b1a1698496bbf7578e49d71
Mykhailo Didur <mihasyaterminator@gmail.com>
2021-06-21 08:02:34 +0300

    Deleted all the code connected with token formatter class because it is depracticed

--- test/gTest.cpp
+++ test/gTest.cpp
@@ -3,7 +3,6 @@
 #include "gtest/gtest.h"
 #include "LexerTests.h"
 #include "ParserTests.h"
-#include "TokenFormatterTests.h"
 #include "ArgumentParserTests.h"
 #include "ExecutorTests.h"
 #include "ConsoleTests.h"
